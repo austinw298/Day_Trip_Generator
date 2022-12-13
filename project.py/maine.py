571eaf886b896bff3e7a6489e58c63a541f192aa
austinw298 <austinw298@gmail.com>
2022-12-13 08:16:54 -0700

    Trial and error to find out my app had glitched and repeated the code no matter what

--- project.py/maine.py
+++ project.py/maine.py
@@ -5,23 +5,47 @@ entertainments = ["Arcade", "Movies", "Concert", "Sports Game"]
 
 import random
 
-destination = random.choice(destinations)
-print(destination)
-user_input = input("Sound Good? y/n: ") 
+
+# while user_input == 'n':
+#     destination = random.choice(destinations)
+#     print(destination)
+#     user_input = input("Sound Good? y/n: ") 
+
+def day_trip(step):
+    user_input = random.choice(step)
+    while user_input == input("n"):
+        part = random.choice(step)
+        print(part)
+        user_input = input("Sound good? y/n ")
+    return
 
 
-if user_input == "n":
-    destination = random.choice(destinations)
-    print(destination)
-else:
-    user_input == "y"
-    print('Next!')
+# def trip_day(step):
+#     user_input = input("start")
+#     if user_input == 'n':
+#         destination = random.choice(step)
+#         print(destination)
+#         user_input = input(f"Does {step} work? ")
+#     elif user_input == 'y':
+#         print('next')
+#         return
+    
+
+# trip_day(destinations)
+
+day_trip(destinations)
+# day_trip(restaurants)
+# day_trip(transportations)
+# day_trip(entertainments)
+
+# if user_input == "n":
+#     destination = random.choice(destinations)
+#     print(destination)
+# else:
+#     user_input == "y"
+#     print('Next!')
 
 
 
 # restaurant = random.choice(restaurants)
 # print(restaurant)
-
-
-# for destination in destinations:
-#     print(destinations)
